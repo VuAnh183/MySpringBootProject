cb5dc0b55c3b14af4c6ce7a26f747521b81daee0
Vu Anh <dinhvuanh2708@gmail.com>
2024-06-12 16:46:49 +0700

    First commit

---
+++ 01-spring-boot-overview/03-actuator-demo/src/test/java/com/vuanh/springboot/demo/myproject/MyprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vuanh.springboot.demo.myproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
